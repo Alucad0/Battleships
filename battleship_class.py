8008eebd9600ace765af372f4abf757ed40f480d
Fredrik Magnevill <96413210+Alucad0@users.noreply.github.com>
2022-05-09 18:09:11 +0200

    Update battleship_class.py

--- battleship_class.py
+++ battleship_class.py
@@ -6,21 +6,20 @@ shapes = [S, Z, I, J, L, T]
 shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), 
                 (138,43,226), (255, 165, 0), (0, 0, 255)]
 
-class Ships(object):
-    # y - because the rows are going to be stacked ontop of each other
-    rows = 20 
-    # x - because the columns are going to be stacked besides each other
-    columns = 10  
+class Shot(object):
+    rows = 10  # y
+    columns = 10  # x
 
     def __init__(self, column, row, shape):
         self.x = column
         self.y = row
         self.shape = shape
-        self.color = shape_colors[shapes.index(shape)]
-        
+        self.color = (255, 255, 255)
+        # number from 0-3
         self.rotation = 0
-        pass
 
+class Piece(Shot):
+    def __init__(self, column, row, shape):
+        super().__init__(column, row, shape)
+        self.color = shape_colors[shapes.index(shape)]
 
-class Board:
-    pass
