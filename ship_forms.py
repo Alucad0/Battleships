5a254b4a0d81b72d5d5487e169b5a1586fd046fe
Fredrik Magnevill <96413210+Alucad0@users.noreply.github.com>
2022-05-18 21:27:14 +0200

    ships.py

--- ship_forms.py
+++ ship_forms.py
@@ -1,5 +1,4 @@
-import random
-import requests
+# shapes
 S = [['.....',
       '.....',
       '..00.',
@@ -96,7 +95,6 @@ T = [['.....',
       '..0..',
       '.....']]
 
-
 empty = [['.....',
       '.....',
       '.....',
@@ -104,38 +102,12 @@ empty = [['.....',
       '.....']]
 
 shapes = [S, Z, I, J, L, T]
-"""
-indexes = []
-while len(indexes) != 6:
-    # generates a random number 0-5 one time repeted
-    url = "http://www.randomnumberapi.com/api/v1.0/random?min=0&max=5&count=1"
-    response = requests.get(url)
-    answer = response.json()
-    if int(answer[0]) not in indexes:
-        indexes.append(int(answer[0]))
 
-temporary = {}
-# shuffles shapes according to indexes
-for key in indexes:
-    for item in shapes:
-        temporary[key] = item
-        shapes.remove(item)
-        break
-shapes = []
-for key in sorted(temporary.keys()):
-    shapes.append(temporary[key])
-"""
 
-# shuffeles because teh api server is unavailable
-random.shuffle(shapes)
-shapes.append(empty)
-
-# index 0 - 6 represent shape
-# color_order: green, red, cyan, violet, orange, blue
 shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), 
-                (138,43,226), (255, 165, 0), (0, 0, 255)]
+                (255, 165, 0), (0, 0, 255), (128, 0, 128), (0, 0, 0)]
+# index 0 - 6 represent shape
 
-# needs to implement this shot 
 bullet = [['.....',
       '.....',
       '..0..',
@@ -146,5 +118,4 @@ bullet = [['.....',
       '..0..',
       '.....',
       '.....']]
-
 
